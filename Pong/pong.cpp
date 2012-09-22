452b1b48a218c9f99b4f275817b5e10af6063c78
Socrates_Dz <sdsxd1800@gmail.com>
2012-09-22 17:59:16 -0400

    Ahora la bola se mueve por la pantalla

--- Pong/pong.cpp
+++ Pong/pong.cpp
@@ -14,7 +14,7 @@ Fecha: 21/09/2012
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_image.h>
+
 
 /*********************** Inicializando Allegro y creando la pantalla ***********/
 
@@ -24,7 +24,6 @@ Fecha: 21/09/2012
 
 int main()
 {
-	ALLEGRO_DISPLAY *display;
 
 	const float FPS = 60.0;
 
@@ -34,7 +33,10 @@ int main()
 		return -1;
 	}
 	
-	display = al_create_display(ScreenWidth, ScreenHeight);
+	al_set_new_display_flags(ALLEGRO_WINDOWED);
+	ALLEGRO_DISPLAY *display = al_create_display(ScreenWidth, ScreenHeight);
+	al_set_window_position(display, 0, 0);
+	al_set_window_title(display, "Pong");
 
 	if(!display)
 	{
@@ -50,34 +52,70 @@ int main()
 	al_init_primitives_addon();
 
 	ALLEGRO_COLOR white = al_map_rgb(255, 255, 255);
-
-	//bool done = false;
+	
+	ALLEGRO_KEYBOARD_STATE keyState;
+	ALLEGRO_TIMER *timer = al_create_timer(1.0/FPS);
+	ALLEGRO_EVENT_QUEUE *eventQueue = al_create_event_queue();
+	al_register_event_source(eventQueue, al_get_keyboard_event_source());
+	al_register_event_source(eventQueue, al_get_timer_event_source(timer));
+	al_register_event_source(eventQueue, al_get_display_event_source(display));
+ 
+	bool done = false;
 	
 	struct Bola
 	{
 		float x;
 		float y;
-		float w;
-		float h;
+		float radius;
+		float moveSpeedX;
+		float moveSpeedY;
 		ALLEGRO_COLOR color;
-	}bola1;
+	}bola;
 
-	bola1.x = bola1.y = bola1.w = bola1.h = 10.0;
-	bola1.color = white;
+	bola.x = ScreenWidth/2;
+	bola.y = ScreenHeight/2;
+	bola.radius = 5.0;
+	bola.color = white;
+	bola.moveSpeedX = 5.0;
+	bola.moveSpeedY = 5.0;
 
 
 	/************************** Principio del juego *******************************/
 
-	//while(!done)
-	//{
-		//al_clear_to_color(al_map_rgb(0, 0, 0));
-		al_draw_filled_rectangle(10, 10, 10, 10, al_map_rgb(255, 255, 255));
+	al_start_timer(timer);
+
+	while(!done)
+	{
+
+		ALLEGRO_EVENT events;
+		al_wait_for_event(eventQueue, &events);
+
+		if(events.type == ALLEGRO_EVENT_KEY_DOWN)
+		{
+			switch(events.keyboard.keycode)
+			{
+				case ALLEGRO_KEY_ESCAPE:
+					done = true;
+			}
+
+		}
+		else if(events.type == ALLEGRO_EVENT_TIMER)
+		{
+			bola.x += bola.moveSpeedX;
+			bola.y += bola.moveSpeedY;
+
+			if(bola.x == ScreenWidth || bola.x == 0) bola.moveSpeedX *= -1;
+			if(bola.y == ScreenHeight || bola.y == 0) bola.moveSpeedY *= -1;
+		}
+
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_draw_filled_circle(bola.x, bola.y, bola.radius, bola.color);
 		al_flip_display();
-		al_rest(5.0);
-		//done = true;
-	//}
+	}
 
 	al_destroy_display(display);
+	al_destroy_event_queue(eventQueue);
+	al_destroy_timer(timer);
 
 	return 0;
 }
