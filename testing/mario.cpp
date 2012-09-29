03910bb5db38167fca70de24cd939bba2c9e8a7e
Socrates_Dz <sdsxd1800@gmail.com>
2012-09-29 08:48:38 -0400

    Arma mejorada (aunque no dispara), tiene un fondo y un enemigo que no se mueve

--- testing/mario.cpp
+++ testing/mario.cpp
@@ -1,3 +1,13 @@
+/*
+
+	Juego de prueba
+
+Aqui definiremos las bases de nuestro juego real.
+
+Autores: Sócrates Díaz, Yordy Aquiles
+
+*/
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_image.h>
@@ -42,7 +52,8 @@ int main()
 
 	ALLEGRO_BITMAP *image = al_load_bitmap("sprites/mario.png");
 	ALLEGRO_BITMAP *mario = al_create_sub_bitmap(image, 3, 603, 16, 21);
-
+	ALLEGRO_BITMAP *fondo = al_load_bitmap("sprites/jordy.jpg");
+	ALLEGRO_BITMAP *malo  = al_load_bitmap ("sprites/enemigo.jpg");
 	ALLEGRO_KEYBOARD_STATE estadoTeclado;
 	ALLEGRO_TIMER *timer = al_create_timer(1.0/FPS);
 	ALLEGRO_EVENT_QUEUE *listaEventos = al_create_event_queue();
@@ -55,19 +66,15 @@ int main()
 	// Ahora si comenzamos con lo del juego
 
 	bool finJuego = false;
-	float velocidad = 8.0;
+	float velocidad = 8.0, rotacion = 360.0;
 	float x = AnchoPantalla/2, y = AltoPantalla/2;
-	int right = 0;
+	int right = 0, distancia_arma = 10;
 
-	ALLEGRO_BITMAP *armas = al_load_bitmap("sprites/weapons2.jpg");
-	ALLEGRO_BITMAP *arma1 = al_create_sub_bitmap(armas, 196, 214, 29, 20);
-	al_convert_mask_to_alpha(arma1, al_map_rgb(255,255,255));
 
-	if(!armas)	// Si no se pudo crear la ventana, entonces pone un mensaje de error
-	{
-		al_show_native_message_box(NULL, "Error", NULL, "No se pudo crear la pantalla", NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return -1;
-	}
+	ALLEGRO_BITMAP *arma = al_load_bitmap("sprites/submachine.png");
+	ALLEGRO_BITMAP *submachine = al_create_sub_bitmap(arma, 4, 4, 49, 21);
+	al_convert_mask_to_alpha(mario, al_map_rgb(0,0,0));
+
 
 	al_start_timer(timer);
 
@@ -77,16 +84,6 @@ int main()
 
 		al_wait_for_event(listaEventos, &eventos);
 
-		// Idea de yordy
-
-	 	/*switch  (eventos.type){
-
-			case ALLEGRO_EVENT_TIMER: 
-				al_get_keyboard_state(&estadoTeclado);
-				switch 
-
-		}*/
-		// Fin de idea de yordy
 
 		if(eventos.type == ALLEGRO_EVENT_TIMER)
 		{
@@ -98,17 +95,27 @@ int main()
 			if(al_key_down(&estadoTeclado, ALLEGRO_KEY_LEFT))
 			{
 				right = 0;
+				distancia_arma = 10;
+				rotacion = 0.0;
 				x -= velocidad;
-			}
+			}		
 			if(al_key_down(&estadoTeclado, ALLEGRO_KEY_RIGHT))
 			{
 				right = 1;
+				distancia_arma = 0;
+				rotacion = 0.0;
 				x += velocidad;
 			}
 			if(al_key_down(&estadoTeclado, ALLEGRO_KEY_UP))
-				y -= velocidad;
+				{
+					rotacion = -90.0;
+					y -= velocidad;
+				}
 			if(al_key_down(&estadoTeclado, ALLEGRO_KEY_DOWN))
-				y += velocidad;
+				{
+					rotacion = 90.0;
+					y += velocidad;
+				}
 
 		if(x <= 0) x = 0;
 		if(x+32 >= AnchoPantalla) x = AnchoPantalla-32;
@@ -119,16 +126,18 @@ int main()
 		}
 
 		al_clear_to_color(al_map_rgb(0, 0, 0));
+		al_draw_scaled_bitmap(fondo, 0, 0, 300, 290, 0, 0, AnchoPantalla, AltoPantalla, 0);
 		al_draw_scaled_bitmap(mario, 0, 0, 16, 21, x, y, 32, 42, right);
-		al_draw_bitmap(arma1, x, y+15, right);
+		al_draw_bitmap(submachine, x - distancia_arma, y+15, right);
+		al_draw_bitmap(malo, 10, 30, 0);
 
 		al_flip_display();
 	}
 
 	al_destroy_bitmap(image);
 	al_destroy_bitmap(mario);
-	al_destroy_bitmap(armas);
-	al_destroy_bitmap(arma1);
+	al_destroy_bitmap(arma);
+	al_destroy_bitmap(submachine);
 	al_destroy_event_queue(listaEventos);
 	al_destroy_display(display);
 	al_destroy_timer(timer);
